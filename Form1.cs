45083bbdbd5e6f2c20b4e47c161424cb6e5ebe88
LICENCIATURAS <LICENCIATURAS@L301-M5>
2024-05-09 11:38:37 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            //La funcion Clear se encarga de borrar el contenido de la Textbox
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            //close se usa para cerrar el programa que esta corriendo 
+            Close();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            //el comando show muestra una ventana con el mensaje "datos guardados"
+            MessageBox.Show("DATOS GUARDADOS");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+
+    class Persona //clase padre
+    {
+        //declaracion de todas las variables que se usaran en el programa y en los hijos
+
+        public string Nombre { get; set; }
+        public string Fecha_Nac { get; set; }
+       
+
+        public string Edad { get; set; }
+        //Constructor
+        /*public Persona(string nombre, string fecha_nac, string carrera_puesto, string edad, int dni_mat, int sueldo)
+        {
+            Nombre = nombre;
+            Fecha_Nac = fecha_nac;
+            Carrera_Puesto = carrera_puesto;
+            Edad = edad;
+            DNI_Mat = dni_mat;
+            Sueldo = sueldo;
+        }*/
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+        //aqui va el constructor para cargar datos en las cajas de memoria
+        //nombre, edad, fecha de nacimiento(es un tipo cadena!!!!! string)
+    }
+
+    class Alumno : Persona //clase hija alumno
+    {
+        public string Carrera { get; set; }
+        public int Matricula { get; set; }
+
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+    class Empleado : Persona //clase hija
+    {
+        public int DNI { get; set; }
+        public int Sueldo { get; set; }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+
+    class Docente : Persona //clase Hija
+    {
+        public int Matricula { get; set; }
+        public int Sueldo { get; set; }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
